3179575e054f85ab519847a636d39dffc5f784ce
JafarMaash <Jmaa831@aucklanduni.ac.nz>
2022-06-27 17:01:26 +1200

    Get all identifiers in a single class

--- src/main/java/JavaParserIdentifiers.java
+++ src/main/java/JavaParserIdentifiers.java
@@ -13,52 +13,60 @@ import com.github.javaparser.resolution.UnsolvedSymbolException;
 public class JavaParserIdentifiers {
     public static void main(String[] args) throws Exception {
         String rootPath = "kalah_designs";
-        String src1 = rootPath + "/design1041";
+        String src1 = rootPath + "/design1041/src/kalah/TestingParser";
         Analyser analyser = new Analyser();
         analyser.addSourcePath(src1);
         Set<Path> paths = Utility.findAllJavaSourceFilesFromRoots(rootPath);
-        for (Path path : paths) {
+
+        // Current hacky move: singling out Kalah class from design 1041 to check its variables
+        // Prints all identifiers declared inside Kalah.java as of right now
+        // Currently commenting out all the visit functionality we don't need: just printing identifier type and name.
+        Path[] pathsList = paths.toArray(new Path[paths.size()]);
+        Path path = pathsList[23];
+//        for (Path path: paths) {
             CompilationUnit compilationUnit = analyser.getCompilationUnitForPath(path);
             VoidVisitor<Object> visitor = new VoidVisitorAdapter<Object>() {
                 @Override
                 public void visit(VariableDeclarator n, Object arg) {
                     super.visit(n, arg);
-                    System.out.println("VariableDeclarator");
-                    System.out.println(" * " + n.getName());
-                    System.out.println(" - " + n);
-                    if (n.resolve().isType()) {
-                        System.out.println("   FQN (type):" + n.resolve().getType());
-                    } else {
-                        System.out.println("   FQN (not type):" + n.resolve().getName());
-                    }
+//                    System.out.println("VariableDeclarator");
+                    System.out.println(n.getType() + ": " + n.getName());
+//                    System.out.println("Type: "+ n.getType());
+//                    System.out.println(" - " + n);
+//                    if (n.resolve().isType()) {
+//                        System.out.println("   FQN (type):" + n.resolve().getType());
+//                    } else {
+//                        System.out.println("   FQN (not type):" + n.resolve().getName());
+//                    }
                 }
 
                 @Override
                 public void visit(VariableDeclarationExpr n, Object arg) {
                     super.visit(n, arg);
                     //JavaParserSimpleFile.listNodes(n);
-                    System.out.println("VariableDeclarationExpr");
-                    System.out.println(" - " + n);
+//                    System.out.println("VariableDeclarationExpr");
+//                    System.out.println(" - " + n);
                 }
 
                 @Override
                 public void visit(PrimitiveType n, Object arg) {
                     super.visit(n, arg);
-                    System.out.println("PrimitiveType: " + n.resolve());
+//                    System.out.println("PrimitiveType: " + n.resolve());
                 }
 
                 @Override
                 public void visit(ClassOrInterfaceType n, Object arg) {
                     super.visit(n, arg);
-                    try {
-                        System.out.println("ClassOrInterfaceType: " + n.resolve().asReferenceType().getQualifiedName());
-                    } catch (UnsolvedSymbolException usex) {
-                        System.out.println("  -> Unrecognised:" + n.getNameAsString());
-                    }
+//                    try {
+//                        System.out.println("ClassOrInterfaceType: " + n.resolve().asReferenceType().getQualifiedName());
+//                    } catch (UnsolvedSymbolException usex) {
+//                        System.out.println("  -> Unrecognised:" + n.getNameAsString());
+//                    }
                 }
             };
             System.out.println("MODULES");
             visitor.visit(compilationUnit, null);
-        }
+//        }
+
     }
 }
