b73be8326a9f9a3063604ceb717b88a40f0e6c92
JafarMaash <Jmaa831@aucklanduni.ac.nz>
2022-09-15 15:44:46 +1200

    Add java language spec rules (where possible)

--- python_parsing/pos_checker.py
+++ python_parsing/pos_checker.py
@@ -16,21 +16,27 @@ with open('test.txt') as file:
         
      
         if identifier_type == "IDENTIFIER": # 2011 B,H,&L POS rule #1 
-            if type != "boolean":
+            if any(token.pos_ == "NOUN" for token in doc):
+                pass
+            else:
+                print("Java spec Violation: fields should have names that are nouns, noun phrases, or abbreviations for nouns", 
+                type + ": " + "'" + identifier + "'")
+                print(identifier)
+
+            if "boolean" not in type:
                 for token in doc:
-                    # print(token.text, token.tag_)
                     if token.tag_ in ["VBZ", "VBP", "VBG", "VB"]:
                         print("Violation: non boolean field with present tense verb |", type + ": " + "'" + identifier + "'")
                         print(identifier) 
             else: 
-                for token in doc:
-                    if token.tag_ in ["MD"]
-                # boolean field names should contain 3person forms of the verb to be or should 
+                if not any(item in ["is", "was", "should"] for item in spiralSplit):
+                    print("Violation: boolean field names should contain 3rd person forms of the verb \"to be\" or \"should\"  |", 
+                    type + ": " + "'" + identifier + "'")
+                    print(identifier) 
 
-
-        
+                
          
-        if identifier_type == "IDENTIFIER" and len(doc) == 1 : # 2011 B,H,&L POS rule #2
+        if identifier_type == "IDENTIFIER" and len(doc) == 1 : # 2011 Binklie et al. "field names should never only be a verb/adjective"
             if doc[0].pos_ == "VERB":
                 print("Violation: field names should never be only a verb |", type + ": " + "'" + identifier + "'")
                 print(identifier) 
@@ -39,8 +45,16 @@ with open('test.txt') as file:
                 print("Violation: field names should never be only a adjective |", type + ": " + "'" + identifier + "'")
                 print(identifier) 
         
+        if identifier_type == "METHOD":
+            if any(token.pos_ == "VERB" for token in doc):
+                    pass
+            else:
+                print("Java spec Violation: method names should be verbs or verb phrases", 
+                type + ": " + "'" + identifier + "'")
+                print(identifier)
 
-         
-        # print(type)
-        # print(split)
-        # print(doc)
+            if "boolean" in type:
+                if spiralSplit[0] != "is":
+                    print("Java spec Violation: methods that test the boolean condition V of an object should be \"isV\" |", 
+                    type + ": " + "'" + identifier + "'")
+                    print(identifier) 
